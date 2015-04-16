250ef1087b04eb6a7d6efcfa432ce18468681d0a
gultaj <yaschick.mail@gmail.com>
2015-04-16 15:40:23 +0300

    add icons, stylus. styling header

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+    stylus: {
+      compile: {
+        // options: {
+        //   paths: ['stylus']
+        // },
+        files: {
+          'css/style.css': 'stylus/style.styl'
+        }
+      }
+    },
+		watch: {
+      stylus: {
+        files: ['stylus/**/*'],
+        tasks: ['stylus:compile'],   // This needs to be "tasks" (not "task")
+        options: { livereload: true }
+      }
+		},
+    dataUri: {
+      dist: {
+        src: ['css/*.css'],
+        dest: 'sample/css',
+        options: {
+          target: ['img/*.*'],
+          baseDir: './'
+        }
+      }
+    }
+	});
+
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.registerTask('default', ['stylus', 'watch']);
+};
